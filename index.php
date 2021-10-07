407128773ef656025166c8f613a614740aa4ad71
Mariano <marianomarchionna8@gmail.com>
2021-10-07 15:24:23 +0200

    Sostituizione parola e verifica lunghezza testo

--- index.php
+++ index.php
@@ -8,9 +8,12 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
     $text = 'Lorem ipsum dolor sit amet consectetur adipisicing ipsum elit. Asperiores harum omnis ipsum inventore! Aspernatur culpa amet optio. Vero deleniti ipsum optio nihil inventore distinctio, ipsum in blanditiis corrupti dolorum tempore ipsum dicta assumenda aut.';
     // $lenght = strlen($text);
     $badWord = $_GET['badWord'];
+    $newText = str_replace($badWord, "***", $text);
 ?>
 
 
 <h1><?php echo $text ?></h1> 
 <h2>Lunghezza paragrafo: <?php echo strlen($text) ?></h2>
 <h2>Parola da censurare: <?php echo $badWord ?></h2>
+<h2><?php echo $newText ?></h2> 
+<h2>Lunghezza paragrafo modificato: <?php echo strlen($newText) ?></h2>
